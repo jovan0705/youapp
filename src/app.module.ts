12dc4f9ecf72b2837679d6508e4581ccfe3cf4ab
jovan <jovan0705@gmail.com>
2024-06-27 11:51:57 +0700

    cleanup and set env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,12 @@ import { MessagesModule } from './messages/messages.module';
 import { RouterModule } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { RabbitMQService } from './rabbitMQ/rabbitmq.service';
-
 @Module({
   imports: [
     ConfigModule.forRoot(),
     UsersModule,
     MessagesModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://jovan:HLBIrUjKvZ960WYS@youapp.zeboxwe.mongodb.net',
-    ),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
     RouterModule.register([
       {
         path: 'api',
